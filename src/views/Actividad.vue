31c82f6f3c4a19103cef457c24d159386e52cc2c
Francisco Vasquez <fjvasquez8@misena.edu.co>
2024-09-24 17:35:36 -0500

    Avance
    Avance

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -17,7 +17,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Opción múltiple
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/AD_CF2/story.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/Actividad_didactica/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
